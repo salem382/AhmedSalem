ce3e2fd92ca2b04403944434f595e12b244d8dd7
ahmed salem <ahmedmohammedsalem382@gmail.com>
2022-08-06 14:48:30 +0200

    updates

--- src/components/mainHead/MainHead.jsx
+++ src/components/mainHead/MainHead.jsx
@@ -1,8 +1,9 @@
+import './main.scss';
 
 const MainHead = ({title1,title2, back}) => {
     return (
 
-         <div className="position-relative text-white py-3">
+         <div className="position-relative text-white py-3 heead">
             <h2 style={{zIndex:'2', top:'40px',fontSize:'calc(5vh + 1rem)'}} className="position-absolute start-50 translate-middle-x fw-bolder text-white d-flex">{title1} <span className="text-yellow ms-3">{title2}</span></h2>
             <p  className="position-absolute fw-bolder start-50 translate-middle-x top-0" style={{fontSize:'calc(9vh + 2rem)',color:'#212121',zIndex:'1'}}>{back}</p>
          </div>
