44cbafc301441d5f3304658577c0dcd44544dfdd
Ahmed <ahmedsalem.official2@gmail.com>
2023-05-13 14:59:47 +0300

    updates

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,24 +5,29 @@ import {Container, Row, Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faCode } from '@fortawesome/free-solid-svg-icons'
 
-const btns = ['All', 'React', 'Frontend', 'javascript'];
+const btns = ['All','Frontend','Backend', 'javascript', 'cpp'];
 const data  = [
-{name: 'Pharmacies', imgSrc:'images/egyptpharmacies.png',codeLink:'https://github.com/salem382/EgyptPharmacies',demoLink:'https://egypt-pharmacies.vercel.app/',depend:['All','React']},
-{name: 'Casher', imgSrc:'images/cashir.png',codeLink:'https://github.com/salem382/Cashir-Program',demoLink:'https://cashir-program.vercel.app/',depend:['All','React']},
+{name: 'Pharmacies', imgSrc:'images/egyptpharmacies.png',codeLink:'https://github.com/salem382/EgyptPharmacies',demoLink:'https://egypt-pharmacies.vercel.app/',depend:['All','Frontend']},
+{name: 'Casher', imgSrc:'images/cashir.png',codeLink:'https://github.com/salem382/Cashir-Program',demoLink:'https://cashir-program.vercel.app/',depend:['All','Frontend']},
 {name: 'Coffe', imgSrc:'images/coffe.png',codeLink:'https://github.com/salem382/coffe-shop',demoLink:'https://coffe-shop-xi.vercel.app/',depend:['All','Frontend']},
 {name: 'Midstr', imgSrc:'images/midstr.png',codeLink:'https://github.com/salem382/midstr',demoLink:'https://midstr-flax.vercel.app/',depend:['All','Frontend']},
 {name: 'Movies fetch', imgSrc:'images/moviesApi.png',codeLink:'https://github.com/salem382/movies-api',demoLink:'https://movies-api-smoky-five.vercel.app/',depend:['All','Frontend']},
-{name: 'Movies React', imgSrc:'images/movies.png',codeLink:'https://github.com/salem382/movies',demoLink:'https://movies-ten-opal.vercel.app/search',depend:['All','React']},
+{name: 'Movies React', imgSrc:'images/movies.png',codeLink:'https://github.com/salem382/movies',demoLink:'https://movies-ten-opal.vercel.app/search',depend:['All','Frontend']},
 {name: 'CRUDS', imgSrc:'images/crud.png',codeLink:'https://github.com/salem382/CRUDS',demoLink:'https://salem382.github.io/CRUDS/',depend:['All','javascript']},
 {name: 'Hangman', imgSrc:'images/hangman.png',codeLink:'https://github.com/salem382/hangman-game',demoLink:'https://salem382.github.io/hangman-game/',depend:['All','javascript']},
 {name: 'Memory', imgSrc:'images/memory.png',codeLink:'https://github.com/salem382/memory_game',demoLink:'https://salem382.github.io/memory_game/',depend:['All','javascript']},
 {name: 'Arabx', imgSrc:'images/arabx.png',codeLink:'https://github.com/salem382/arabx',demoLink:'https://salem382.github.io/arabx/',depend:['All','Frontend']},
 {name: 'Elnagar', imgSrc:'images/elgar.png',codeLink:'https://github.com/salem382/Elnagar',demoLink:'https://salem382.github.io/Elnagar/',depend:['All','Frontend']},
 {name: 'Fokir', imgSrc:'images/fokir.png',codeLink:'https://github.com/salem382/Fokir',demoLink:'https://salem382.github.io/Fokir/',depend:['All','Frontend']},
-{name: 'Facebook Registration', imgSrc:'images/facebook.png',codeLink:'https://github.com/salem382/Facebook-Registration',demoLink:'https://facebook-registration.vercel.app/',depend:['All','javascript']},
 {name: 'Book List', imgSrc:'images/boook.png',codeLink:'https://github.com/salem382/Book-store',demoLink:'https://github.com/salem382/Book-store',depend:['All','React']},
-{name: 'Card Shop', imgSrc:'images/card.png',codeLink:'https://github.com/salem382/card-shop',demoLink:'https://github.com/salem382/card-shop',depend:['All','React']},
-{name: 'Dashboard', imgSrc:'images/dashboard.png',codeLink:'https://github.com/salem382/Dashboard',demoLink:'https://dashboard-self-two.vercel.app/',depend:['All','React']},
+{name: 'Ecommerce Website', imgSrc:'images/eco.png',codeLink:'https://github.com/salem382/nodeJs-noon-clone',demoLink:'https://github.com/salem382/nodeJs-noon-clone',depend:['All','Backend']},
+{name: 'Middle School', imgSrc:'images/middle.png',codeLink:'https://github.com/salem382/nodeJs-Middle-School',demoLink:'https://github.com/salem382/nodeJs-Middle-School',depend:['All','Backend']},
+{name: 'high School', imgSrc:'images/high.png',codeLink:'https://github.com/salem382/nodJs-high-school',demoLink:'https://github.com/salem382/nodJs-high-school',depend:['All','Backend']},
+{name: 'Library system', imgSrc:'images/lib.jpg',codeLink:'https://github.com/salem382/nodeJs_librarySystem',demoLink:'https://github.com/salem382/nodeJs_librarySystem',depend:['All','Backend']},
+{name: 'Snake Game', imgSrc:'images/snake.png',codeLink:'https://github.com/salem382/cpp-snake-game',demoLink:'https://github.com/salem382/cpp-snake-game',depend:['All','cpp']},
+{name: 'Ping pong Game', imgSrc:'images/pong.jpg',codeLink:'https://github.com/salem382/cpp-pingPong-game',demoLink:'https://github.com/salem382/cpp-pingPong-game',depend:['All','cpp']},
+{name: 'Cross Road Game', imgSrc:'images/cross.png',codeLink:'https://github.com/salem382/cpp-crossRoad-game',demoLink:'https://github.com/salem382/cpp-crossRoad-game',depend:['All','cpp']},
+{name: 'Ask me System', imgSrc:'images/ask.png',codeLink:'https://github.com/salem382/cpp-askMe-system',demoLink:'https://github.com/salem382/cpp-askMe-system',depend:['All','cpp']},
 ];
 
 const Portfolio = () => {
